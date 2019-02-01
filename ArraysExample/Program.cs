7641c0053dbf102e8383bc5d2b34db0888f664b9
Nick <nbove@mail.usf.edu>
2019-01-31 23:07:28 -0500

    version1

--- ArraysExample/Program.cs
+++ ArraysExample/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/* Author: Nick Bove
+   Date: 1/31/19
+   Comments: This C# Console Application demosntrates the use of arrays
+             without user input
+*/
+using System;
 
 namespace ArraysExample
 {
@@ -6,7 +11,23 @@ namespace ArraysExample
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			try
+			{
+				int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+
+				foreach (int i in numbers)
+				{
+					Console.WriteLine(i + " ");
+					Console.ReadKey(true);
+				}
+
+
+			}
+			catch
+			{
+				Console.WriteLine("Press any key to exit the program and try again ...");
+				Console.ReadKey(true);
+			} 
 		}
 	}
 }
